ab43c1e5f9a82f0c5fb1f557e9e2ec3efcfd16bd
xiangzhi <xiangzhi@126.com>
2020-02-16 20:33:59 +0800

    单元测试

--- dingtalk_test.go
+++ dingtalk_test.go
@@ -1,1 +1,17 @@
 package dingtalk
+
+import "testing"
+
+func Test_accessToken_GetToken(t *testing.T) {
+
+    Option.AppKey = "dingyirbacim1xgtfrcq"
+    Option.AppSecret="hwAk3apBb3kVGEawMzuYFPUvxn_VBMCeb9FtO3LYTfy280Qp3ZXZ3UHcD2zMR_3L"
+
+    token,err:=AccessToken.GetToken()
+    if err != nil {
+        t.Error(err)
+        return
+    }
+
+    t.Log(token)
+}
